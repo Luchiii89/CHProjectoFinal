9ce19350c865dce30e18a9e950c8d329458ca7cb
luchosanjuan <luchogrossi@gmail.com>
2022-07-19 15:18:52 -0300

    "Agregado Campos y tabla"

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -1,3 +1,4 @@
+from telnetlib import DO
 from django.db import models
 
 # Create your models here.
@@ -5,23 +6,43 @@ from django.db import models
 class Patient(models.Model):
     name = models.CharField(max_length=60)
     surname = models.CharField(max_length=60)
-    genre = models.CharField(max_length=60)
+    
+    genre = [
+        ('F', 'Femenino'),
+        ('M', 'Masculino')
+    ]
+    genre = models.CharField(max_length=1,choices=genre, default='F')
+
     patId = models.IntegerField()
     mail = models.EmailField()
     tel = models.IntegerField()
     address = models.CharField(max_length=60)
 
+    def __str__(self):
+        txt="{0}, {1}"
+        return txt.format(self.name, self.surname, self.genre, self.patId)
+        #Luciana despues aca agregamos a los demas lo mismo para que se vea mas bonito.
+
 class Department(models.Model):
     depId = models.IntegerField()
     name = models.CharField(max_length=40)
     mail = models.EmailField()
-    tel = models.IntegerField()
-    head = models.CharField(max_length=60)
+    tel = models.IntegerField()   
+
+    def __str__(self):
+        txt="Dpto. ID: {0} / Name: {1} / Celphone: {2} / HeadDepartment: {3}"
+        return txt.format(self.depId, self.name, self.tel, self.head)
 
 class Doctor(models.Model):
     name = models.CharField(max_length=60)
     surname = models.CharField(max_length=60)
-    genre = models.CharField(max_length=60)
+
+    genre = [
+        ('F', 'Femenino'),
+        ('M', 'Masculino')
+    ]
+    genre = models.CharField(max_length=1,choices=genre, default='F')
+
     docId = models.IntegerField()
     license = models.CharField(max_length=20)
     mail = models.EmailField()
@@ -31,10 +52,9 @@ class Doctor(models.Model):
     department = Department.name
 
     def __str__(self):
-        txt="{0}, {1}"
-        return txt.format(self.name, self.surname)
+        txt="{0} / {1} / {2} / {3} / {4}"
+        return txt.format(self.name, self.surname, self.genre, self.docId, self.department)
         #Luciana despues aca agregamos a los demas lo mismo para que se vea mas bonito.
-
 
 class History(models.Model):
     patient = Patient.name
