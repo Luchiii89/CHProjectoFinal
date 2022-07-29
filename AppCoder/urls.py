6ee33eb7dab49d837a314209939b6c6ba0cfb42f
Your Name <you@example.com>
2022-07-29 16:16:16 -0300

    Fix getdoctorBySurname

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -9,13 +9,11 @@ urlpatterns = [
    
     path('', views.Start.as_view(), name="Start"),
     path('newDoctor/', views.newDoctor.as_view(), name="newDoctor"),
-    path('newPatient/', views.newPatient, name="newPatient"),
-    #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
-    
+    path('newPatient/', views.newPatient.as_view(), name="newPatient"),
     path('listDoctor/', views.ListDoctor.as_view(), name="listDoctor"), #Clase Vista, name="listDoctor" ->tiene que coincidir con el TP
     path('listPatient/', views.ListPatient.as_view(), name="listPatient"),
-
-    path('getDoctorBySurname/', views.getDoctorBySurname.as_view(), name="AS"),
+    path('doctorBySurname/', views.getDoctorBySurname.as_view(), name="doctorBySurname"),
+    #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)    
     #path('app_contactos/', ContactoListar.as_view(template_name = "app_contactos/index.html"), name='listar'),
     #path('deleteDoctor/<int:pk>', views.deleteDoctor, name="deleteDoctor"), #Eliminamos 
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,17 +1,20 @@
-from multiprocessing import context
-from pydoc import Doc
 from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from django.http.request import QueryDict
 from django.http import HttpResponse
-from numpy import result_type
-from yaml import DocumentStartToken
-from AppCoder import models
-from AppCoder.models import Patient, Department, Doctor, History
-from AppCoder.forms import *
 from django.db.models import Q
 from django.views.generic import ListView, View, TemplateView, UpdateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+
+from multiprocessing import context
+from pydoc import Doc
+from numpy import result_type
+from yaml import DocumentStartToken
+
+from AppCoder import models
+from AppCoder.models import Patient, Department, Doctor, History
+from AppCoder.forms import *
+
 
 
 # Create your views here.
@@ -31,28 +34,29 @@ class ListDoctor(ListView):
 class newDoctor(CreateView):
     template_name = "AppCoder/newDoctor.html"
     
+class newPatient(CreateView):
+    template_name = "AppCoder/newPatient.html"
 
-
-# def newDoctor(request):
-#     if request.method == 'POST':
-#         miFormulario = DoctorForm(request.POST) #aquí mellega toda la información del html
-#         print(miFormulario)
-#         if miFormulario.is_valid:   #Si pasó la validación de Django
-#             informacion = miFormulario.cleaned_data
-#             doctor = Doctor(name=informacion['name'], 
-#                         surname=informacion['surname'], 
-#                         genre=informacion['genre'], 
-#                         docId=informacion['docId'],
-#                         license=informacion['license'],
-#                         mail=informacion['mail'],
-#                         tel=informacion['tel'],
-#                         address=informacion['address'],
-#                         specialization=informacion['specialization']) 
-#             doctor.save()
-#             return render(request, "AppCoder/index.html") #Vuelvo al inicio o a donde quieran
-#     else: 
-#         miFormulario = DoctorForm() #Formulario vacio para construir el html
-#     return render(request, "AppCoder/newDoctor.html", {"miFormulario":miFormulario})
+def newDoctor(request):
+    if request.method == 'POST':
+        miFormulario = DoctorForm(request.POST) #aquí mellega toda la información del html
+        print(miFormulario)
+        if miFormulario.is_valid:   #Si pasó la validación de Django
+            informacion = miFormulario.cleaned_data
+            doctor = Doctor(name=informacion['name'], 
+                        surname=informacion['surname'], 
+                        genre=informacion['genre'], 
+                        docId=informacion['docId'],
+                        license=informacion['license'],
+                        mail=informacion['mail'],
+                        tel=informacion['tel'],
+                        address=informacion['address'],
+                        specialization=informacion['specialization']) 
+            doctor.save()
+            return render(request, "AppCoder/index.html") #Vuelvo al inicio o a donde quieran
+    else: 
+        miFormulario = DoctorForm() #Formulario vacio para construir el html
+    return render(request, "AppCoder/newDoctor.html", {"miFormulario":miFormulario})
 
 def newPatient(request):
     if request.method == 'POST':
@@ -81,4 +85,4 @@ class ListPatient(ListView): #ListPatient es el que se le pasa a URL con el .as_
 
 class getDoctorBySurname(ListView):        #incompleto
     queryset = Doctor.objects.filter(surname__icontains='war')
-    template_name = "AppCoder/getDoctor.html"
+    template_name = "AppCoder/doctorBySurname.html"
