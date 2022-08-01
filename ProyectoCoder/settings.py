5e82da81b11baacf8f7d6b3e6a8ce8de92ba7f58
Your Name <you@example.com>
2022-08-01 19:46:31 -0300

    Fix index.html

--- ProyectoCoder/settings.py
+++ ProyectoCoder/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProyectoCoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'AppCoder/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
